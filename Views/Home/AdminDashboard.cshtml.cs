2bcc4a12ffb5fd1cffaa777c8781b4de6d828a52
Broc Keeton <broc.keeton@edgernd.com>
2025-03-13 12:35:07 -0700

    admindashboard routing works

---
+++ Views/Home/AdminDashboard.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using GourmetShopMVC.Data;
+using GourmetShopMVC.Models;
+
+namespace GourmetShopMVC.Views.Home
+{
+    public class AdminDashboardModel : PageModel
+    {
+        private readonly GourmetShopMVC.Data.GourmetShopMVCContext _context;
+
+        public AdminDashboardModel(GourmetShopMVC.Data.GourmetShopMVCContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product.ToListAsync();
+        }
+    }
+}
