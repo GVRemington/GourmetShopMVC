c37b44e746f5abbfaac790351dea322e820749db
Sarah Harlan <baby.blue.kitten1013@gmail.com>
2025-03-17 14:35:47 -0700

    Finished checkout controller

--- Controllers/CheckoutController.cs
+++ Controllers/CheckoutController.cs
@@ -33,16 +33,48 @@ namespace GourmetShopMVC.Controllers
             return RedirectToAction("Index");
         }
 
-        public ActionResult Checkout()
+        public async Task<ActionResult> Checkout()
         {
 
+            var orderId = 0;
+
             // use the context to create a order, getting us an order id
+            Order order = new Order
+            {
+                Id = 0,
+                OrderDate = DateTime.Now,
+                OrderNumber = null,
+                UserId = 92,
+                TotalAmount = null,
+                OrderCancelled = false,
+            };
+
+            _context.Order.Add(order);
+            await _context.SaveChangesAsync();
+
+            orderId = order.Id;
+
+            decimal Subtotal = 0;
 
             // use the context to take our cartservice.cart and update all orderitems with the order id
+            foreach( OrderItem item in _cartService.Cart)
+            {
+                item.OrderId = orderId;
+
+                Subtotal += item.UnitPrice * item.Quantity;
+
+                _context.OrderItem.Add(item);
+            }
+
+            order.TotalAmount = Subtotal;
+
+            await _context.SaveChangesAsync();
 
             // use the context to create orderitem rows in the database (from the carservice.cart items)
 
-            return NotFound();
+            _cartService.EmptyCart();
+
+            return RedirectToAction("Index", "Shop");
         }
     }
 }
